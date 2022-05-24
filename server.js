e026fb9cc321d0e390782cf4550b5a6f6a9c6b93
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-05-23 21:01:06 -0400

    finish music tour api

--- server.js
+++ server.js
@@ -2,21 +2,8 @@
 const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
+const bands = require('./controllers/bands_controller')
 
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize({
-  storage: process.env.PG_URI,
-  dialect: 'postgres',
-  username: 'postgres',
-  password: 'agilebeast',
-})
-
-try {
-  sequelize.authenticate()
-  console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
-} catch {
-  console.log(`Unable to connect to PG ${err}`)
-}
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
@@ -28,6 +15,16 @@ app.get('/', (req, res) => {
     message: 'Welcome to the Tour API',
   })
 })
+
+// CONTROLLERS
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+const eventsController = require('./controllers/events_controller')
+app.use('/events', eventsController)
+
+const stagesController = require('./controllers/stages_controller')
+app.use('/stages', stagesController)
 
 // LISTEN
 app.listen(process.env.PORT, () => {
